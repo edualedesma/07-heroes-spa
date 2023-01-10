74102c9406bab3e00cb4ab2a3cfd0089993083a5
Eduardo Acuña <eaculed@gmail.com>
2023-01-10 14:36:05 +0000

    Improvements on search page

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -13,13 +13,16 @@ export const SearchPage = () => {
   const { q = '' } = queryString.parse(location.search);
   const heroes = getHeroesByName(q);
 
+  const showSearch = ( q.length === 0 );
+  const showError = ( q.length > 0 && heroes.length === 0 );
+
   const { searchText, onInputChange } = useForm({
-    searchText: ''
+    searchText: q
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if ( searchText.trim().length <= 1 ) return;
+    // if ( searchText.trim().length <= 1 ) return;
     // console.log({searchText});
 
     navigate(`?q=${ searchText }`);
@@ -53,10 +56,26 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-          <div className="alert alert-primary">
+
+          {/* Forma 1 */}
+          {/* {
+            ( q === '' )
+              ? <div className="alert alert-primary">Search a hero</div>
+              : ( heroes.length === 0 )
+              && <div className="alert alert-danger">No hero with <b>{ q }</b></div>
+          } */}
+
+          {/* Forma 2 */}
+          <div 
+            className="alert alert-primary animate__animated animate__fadeIn" 
+            style={{ display: showSearch ? '' : 'none' }}
+          >
             Search a hero
-          </div>
-          <div className="alert alert-danger">
+          </div>  
+          <div 
+            className="alert alert-danger animate__animated animate__fadeIn" 
+            style={{ display: showError ? '' : 'none' }}
+          >
             No hero with <b>{ q }</b>
           </div>
 
