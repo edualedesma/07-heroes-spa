f7268228a987e80dc4aa99005ed70b81c43e2388
Eduardo Acuña <eaculed@gmail.com>
2023-01-18 23:28:36 +0000

    Creating AuthContext with reducer and provider

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,10 +1,11 @@
+import { AuthProvider } from "./auth";
 import { AppRouter } from "./routes/AppRouter";
 
 
 export const HeroesApp = () => {
     return (
-        <>
+        <AuthProvider>
             <AppRouter />
-        </>
+        </AuthProvider>
     )
 }
